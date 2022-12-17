a6567035c3cf83d69335f52e2c74de770dd1ff05
YosefBakry <youssuf.bakry@gmail.com>
2022-12-17 11:01:05 +0300

    Add existing project files to Git before react

---
+++ v1/myScript.js
@@ -0,0 +1,112 @@
+// Selecting DOM elements
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+const factsList = document.querySelector(".facts-list");
+
+factsList.innerHTML = "";
+
+//Loading from supabase
+
+loadFacts();
+async function loadFacts() {
+  const response = await fetch(
+    "https://baarcbwedjhmqirubnws.supabase.co/rest/v1/facts",
+    {
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhYXJjYndlZGpobXFpcnVibndzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA5NDU5MjgsImV4cCI6MTk4NjUyMTkyOH0.CEdTdV7pOmFP06uy1gytrTpJ_t5gHl2e8jxi0Vu5AFk",
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhYXJjYndlZGpobXFpcnVibndzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA5NDU5MjgsImV4cCI6MTk4NjUyMTkyOH0.CEdTdV7pOmFP06uy1gytrTpJ_t5gHl2e8jxi0Vu5AFk",
+      },
+    }
+  );
+  const data = await response.json();
+  console.log(data);
+  const filteredData = data.filter((data) => data.category === "technology");
+  createFactsList(filteredData);
+}
+
+function createFactsList(dataArray) {
+  // factsList.insertAdjacentHTML("afterbegin", "<li>Jonas</li>");
+
+  const htmlArr = dataArray.map(
+    (fact) => `<li class="fact">
+    <p>
+    ${fact.text}
+      <a
+        class="source"
+        href="${fact.source}"
+        target="_blank"
+      >(Source)</a>
+    </p>
+    <span class="tag" style="background-color: ${
+      CATEGORIES.find((cat) => cat.name === fact.category).color
+    }">${fact.category}</span>
+  </li>`
+  );
+  const html = htmlArr.join("");
+  factsList.insertAdjacentHTML("afterbegin", html);
+}
+btn.addEventListener("click", () => {
+  form.classList.contains("hidden")
+    ? (form.classList.remove("hidden"), (btn.textContent = "hide"))
+    : (form.classList.add("hidden"), (btn.textContent = "share a fact"));
+});
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+const allCategories = CATEGORIES.map((el) => el.name);
+// console.log(allCategories);
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+
+  if (age >= 0) return age;
+  else return `Impossible year. Year needs to be less or equal ${currentYear}`;
+}
+
+const facts = initialFacts.map((obj) => calcFactAge(obj.createdIn));
